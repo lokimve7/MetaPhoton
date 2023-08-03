62dda93134c96f346e5b35bf06b745be2cb5b0bd
lokimve7 <lokimve7@gmail.com>
2023-08-03 13:55:06 +0900

    .

--- Assets/01_Scripts/PlayerFire.cs
+++ Assets/01_Scripts/PlayerFire.cs
@@ -26,27 +26,21 @@ public class PlayerFire : MonoBehaviourPun
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
             //FireBulletByInstantiate();
-            photonView.RPC(nameof(FireBulletByRpc), RpcTarget.All);
+
+            //만들어진 폭탄을 카메라 앞방향으로 1만큼 떨어진 지점에 놓는다.
+            Vector3 pos = Camera.main.transform.position + Camera.main.transform.forward * 1;
+
+            //만들어진 총알의 앞방향을 카메라가 보는 방향으로 설정
+            Vector3 forward = Camera.main.transform.forward;
+
+            photonView.RPC(nameof(FireBulletByRpc), RpcTarget.All, pos, forward);
         }
 
         //2번키 누르면
         if(Input.GetKeyDown(KeyCode.Alpha2))
         {
-            //카메라위치, 카메라 앞방향으로 Ray 를 만들자.
-            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-            //만약에 Ray 를 발사해서 부딪힌 곳이 있다면
-            RaycastHit hitInfo;
-            if(Physics.Raycast(ray, out hitInfo))
-            {
-                //그 위치에 파편효과공장에서 파편효과를 만든다.
-                GameObject fragment = Instantiate(fragmentFactory);
-                //만든어진 파편효과를 부딪힌 위치에 놓는다.
-                fragment.transform.position = hitInfo.point;
-                //파편효과의 방향을 부딪힌 위치의 normal 방향으로 설정
-                fragment.transform.forward = hitInfo.normal;
-                //2초 뒤에 파편효과를 파괴하자
-                Destroy(fragment, 2);
-            }
+            photonView.RPC(nameof(FireRayByRpc), RpcTarget.All, 
+                Camera.main.transform.position, Camera.main.transform.forward);
         }
     }
 
@@ -63,16 +57,31 @@ public class PlayerFire : MonoBehaviourPun
     }
 
     [PunRPC]
-    void FireBulletByRpc()
+    void FireBulletByRpc(Vector3 firePos, Vector3 fireFoward)
     {
-        //만들어진 폭탄을 카메라 앞방향으로 1만큼 떨어진 지점에 놓는다.
-        Vector3 pos = Camera.main.transform.position + Camera.main.transform.forward * 1;
-
-        //만들어진 총알의 앞방향을 카메라가 보는 방향으로 설정
-        Quaternion rot = Camera.main.transform.rotation;
 
         GameObject bomb = Instantiate(bombFactory);
-        bomb.transform.position = pos;
-        bomb.transform.rotation = rot;
+        bomb.transform.position = firePos;
+        bomb.transform.forward = fireFoward;
+    }
+
+    [PunRPC]
+    void FireRayByRpc(Vector3 firePos, Vector3 fireFoward)
+    {
+        //카메라위치, 카메라 앞방향으로 Ray 를 만들자.
+        Ray ray = new Ray(firePos, fireFoward);
+        //만약에 Ray 를 발사해서 부딪힌 곳이 있다면
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            //그 위치에 파편효과공장에서 파편효과를 만든다.
+            GameObject fragment = Instantiate(fragmentFactory);
+            //만든어진 파편효과를 부딪힌 위치에 놓는다.
+            fragment.transform.position = hitInfo.point;
+            //파편효과의 방향을 부딪힌 위치의 normal 방향으로 설정
+            fragment.transform.forward = hitInfo.normal;
+            //2초 뒤에 파편효과를 파괴하자
+            Destroy(fragment, 2);
+        }
     }
 }
