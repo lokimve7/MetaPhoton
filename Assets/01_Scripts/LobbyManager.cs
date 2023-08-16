17fb46d65fbfa9d959eaa9116aa2c17bd38b84f8
lokimve7 <lokimve7@gmail.com>
2023-08-16 13:53:49 +0900

    .

--- Assets/01_Scripts/LobbyManager.cs
+++ Assets/01_Scripts/LobbyManager.cs
@@ -11,6 +11,9 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public InputField inputRoomName;
     //Input Max Player
     public InputField inputMaxPlayer;
+    //Input Password
+    public InputField inputPassword;
+
     //방 참여 버튼
     public Button btnJoinRoom;
     //방 생성 버튼
@@ -60,12 +63,28 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         //방에 참여할 수 있니? 없니?
         option.IsOpen = true;
 
+        //custom 설정
+        ExitGames.Client.Photon.Hashtable hash = new ExitGames.Client.Photon.Hashtable();
+        hash["room_name"] = inputRoomName.text;
+        hash["map_idx"] = 10;
+        hash["use_item"] = true;
+
+        //custom 설정을 option 에 셋팅
+        option.CustomRoomProperties = hash;
+
+        //custom 정보를 Lobby 에서 사용할 수 있게 설정
+        string [] customKeys = { "room_name", "map_idx", "use_item" };
+        option.CustomRoomPropertiesForLobby = customKeys;
+
+
+
+
         //특정 로비에 방 생성 요청
         //TypedLobby typedLobby = new TypedLobby("Meta Lobby", LobbyType.Default);
         //PhotonNetwork.CreateRoom(inputRoomName.text, option, typedLobby);
         
         //기본 로비에 방 생성 요청
-        PhotonNetwork.CreateRoom(inputRoomName.text, option);
+        PhotonNetwork.CreateRoom(inputRoomName.text + inputPassword.text , option);
     }
 
     //방 생성 완료시 호출 되는 함수
@@ -85,7 +104,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public void JoinRoom()
     {
         //방 입장 요청
-        PhotonNetwork.JoinRoom(inputRoomName.text);
+        PhotonNetwork.JoinRoom(inputRoomName.text + inputPassword.text);
     }
 
     // 방 입장 완료시 호출되는 함수
@@ -154,10 +173,22 @@ public class LobbyManager : MonoBehaviourPunCallbacks
             ////만들어진 roomItem 의 부모를 scrollView -> Content 의 transform 으로 한다.
             //goRoomItem.transform.parent = rtContent;
 
+            //custom 정보 뽑아오자.
+            string roomName = (string)(info.CustomProperties["room_name"]);
+            int mapIdx = (int)(info.CustomProperties["map_idx"]);
+            bool useItme = (bool)(info.CustomProperties["use_item"]);
+
             //만들어진 roomItem 에서 RoomItem 컴포넌트 가져온다.
             RoomItem roomItem = goRoomItem.GetComponent<RoomItem>();
             //가져온 컴포넌트가 가지고 있는 SetInfo 함수 실행
-            roomItem.SetInfo(info.Name, info.PlayerCount, info.MaxPlayers);
+            roomItem.SetInfo(roomName, info.PlayerCount, info.MaxPlayers);
+            //RoomItem 이 클릭 되었을 때 호출되는 함수 등록
+            roomItem.onChangeRoomName = OnChangeRoomNameField;
+
+            //람다식 lamda 
+            //roomItem.onChangeRoomName = (string roomName) => {
+            //    inputRoomName.text = roomName;
+            //};
         }
     }
 
@@ -177,6 +208,10 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         
     }
 
+    public void OnChangeRoomNameField(string roomName)
+    {
+        inputRoomName.text = roomName;
+    }
 
     void Update()
     {
--- Assets/01_Scripts/RoomItem.cs
+++ Assets/01_Scripts/RoomItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,10 @@ using UnityEngine.UI;
 public class RoomItem : MonoBehaviour
 {
     public Text roomInfo;
+
+    // 클릭 되었을 때 호출 해줄 함수를 담을 변수
+    public Action<string> onChangeRoomName;
+
     void Start()
     {
         
@@ -28,11 +33,18 @@ public class RoomItem : MonoBehaviour
 
     public void OnClick()
     {
-        //1. InputRoomName 게임오브젝트 찾자.
-        GameObject go = GameObject.Find("InputRoomName");
-        //2. 찾은 게임오브젝트에서 InputField 컴포넌 가져오자
-        InputField inputField = go.GetComponent<InputField>();
-        //3. 가져온 컴포넌트를 이용해서 Text 값 변경
-        inputField.text = name;
+        //onChangeRoomName 가 null 이 아니라면
+        if(onChangeRoomName != null)
+        {
+            onChangeRoomName(name);
+        }
+
+
+        ////1. InputRoomName 게임오브젝트 찾자.
+        //GameObject go = GameObject.Find("InputRoomName");
+        ////2. 찾은 게임오브젝트에서 InputField 컴포넌 가져오자
+        //InputField inputField = go.GetComponent<InputField>();
+        ////3. 가져온 컴포넌트를 이용해서 Text 값 변경
+        //inputField.text = name;
     }
 }
